c959485f64fe36c3dbeed87a0090d2455237bf05
thaisbreder <thais.breder@zup.com.br>
2023-05-26 17:45:59 -0300

    :sparkles: exercicio 02 finalizado

---
+++ src/E02.java
@@ -0,0 +1,8 @@
+public class E02 {
+    public static void main(String[] args) {
+        System.out.println("******** CONTADOR DECRESCENTE ********");
+        for(int i = 100 ; i >= 1 ; i--){
+            System.out.println("A contador está em " + i);
+        }
+    }
+}
